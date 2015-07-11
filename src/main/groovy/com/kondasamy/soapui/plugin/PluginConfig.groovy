dd836687273613702c9648d1280e1df87fa9fe93
Kondasamy Jayaraman <jkondasamy1992@gmail.com>
2015-07-12 00:41:47 +0530

    SoapUI OpenSource/ SoapUI Pro / Ready! API Plugin - to export results (Raw Request and Response)

---
+++ src/main/groovy/com/kondasamy/soapui/plugin/PluginConfig.groovy
@@ -0,0 +1,12 @@
+package com.kondasamy.soapui.plugin;
+
+import com.eviware.soapui.plugins.PluginAdapter;
+import com.eviware.soapui.plugins.PluginConfiguration;
+
+@PluginConfiguration(groupId = "com.kondasamy.soapui.plugin.plugins", name = "SoapUI/ Ready! API : Export Results plugin", version = "1.1",
+        autoDetect = true, description = "SoapUI/ Ready! API : Export Results plugin(SoapUI Actions) - Kondasamy",
+        infoUrl = "www.kondasamy.com" )
+public class PluginConfig extends PluginAdapter
+{
+
+}
