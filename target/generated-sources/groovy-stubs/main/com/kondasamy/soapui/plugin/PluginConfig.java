dfc2498e263abd15616d28aca18ce4b91a7c913f
Kondasamy Jayaraman <jkondasamy1992@gmail.com>
2015-10-30 22:37:18 +0530

    Changes implemented and removed dummy codes

--- target/generated-sources/groovy-stubs/main/com/kondasamy/soapui/plugin/PluginConfig.java
+++ target/generated-sources/groovy-stubs/main/com/kondasamy/soapui/plugin/PluginConfig.java
@@ -7,7 +7,7 @@ import java.util.*;
 import groovy.lang.*;
 import groovy.util.*;
 
-@com.eviware.soapui.plugins.PluginConfiguration(infoUrl="www.kondasamy.com", groupId="com.kondasamy.soapui.plugin.plugins", autoDetect=true, name="SoapUI/ Ready! API : Export Results plugin", description="SoapUI/ Ready! API : Export Results plugin(SoapUI Actions & Listener) - Kondasamy", version="1.3") public class PluginConfig
+@com.eviware.soapui.plugins.PluginConfiguration(infoUrl="www.kondasamy.com", groupId="com.kondasamy.soapui.plugin.plugins", autoDetect=true, name="SoapUI/ Ready! API : Export Results plugin", description="SoapUI/ Ready! API : Export Results plugin(SoapUI Actions & Listener) - Kondasamy", version="1.4") public class PluginConfig
   extends com.eviware.soapui.plugins.PluginAdapter  implements
     groovy.lang.GroovyObject {
 public  groovy.lang.MetaClass getMetaClass() { return (groovy.lang.MetaClass)null;}
